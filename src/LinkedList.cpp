045ae7da9f848310c310d0c2500e0b247fe65ae9
OzgurTY <ozgur.yavuz@ozu.edu.tr>
2023-03-06 10:34:43 +0300

    Added LinkedList and Node classes.

---
+++ src/LinkedList.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ozgur on 6.03.2023.
+//
+
+#include "LinkedList.h"
+
+LinkedList::LinkedList() {
+    head = nullptr;
+    tail = nullptr;
+}
---
+++ src/Node.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ozgur on 6.03.2023.
+//
+
+#include "Node.h"
+
+Node::Node(int data) {
+    this->data = data;
+    this->next = nullptr;
+}
